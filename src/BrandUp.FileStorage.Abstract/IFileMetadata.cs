54298c864fb90c198457cf93c41758fbef1c2777
Дмитрий Шашев <DmitryShashev@outlook.com>
2023-02-10 18:07:24 +0700

    Fix mistake

--- src/BrandUp.FileStorage.Abstract/IFileMetadata.cs
+++ src/BrandUp.FileStorage.Abstract/IFileMetadata.cs
@@ -5,10 +5,7 @@
     /// </summary>
     public interface IFileMetadata
     {
-        /// <summary>
-        /// Id of the file that can be used to get the file from storage
-        /// </summary>
-        public Guid FileId { get; init; }
+
         /// <summary>
         /// Name of file
         /// </summary>
